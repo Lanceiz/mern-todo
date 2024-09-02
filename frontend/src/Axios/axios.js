c0a9ce863bb180814e355db37556567cc5682bbb
Lanceiz <atuls.90601@gmail.com>
2024-09-02 11:55:43 +0530

    deployed

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://mern-todo-eight-beta.vercel.app/"
+    baseURL:"https://mern-todo-q9gc.vercel.app/"
 })
 export default instance
